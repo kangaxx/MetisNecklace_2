22fe0046366c4500aa44c716ca54d0a31816a01b
root <root@iZ2ze3pjjhxv6g0twxdd7dZ.(none)>
2019-05-21 14:12:05 +0800

    ... ...

---
+++ src/main/java/glx/cn/sh/protoHelper.java
@@ -0,0 +1,46 @@
+package glx.cn.sh;
+import glx.cn.sh.TestData.*;
+/**
+ *
+ * protobuf functions
+ *
+**/
+
+public class protoHelper
+{
+  public  protoHelper(){}
+
+  public interface pbSerializer{
+    public void initial(byte[] bytes); 
+    public String getDspName();
+//    public void setDspName(String name);
+//    public int getRtbId();
+  }
+
+  public interface pbDeserializer{
+    public void initial(Object obj, Class cls);
+    public String getDspName();
+    public void setDspName(String name);
+    public int getRtbId();
+  }
+
+  public interface pbFactory{
+    public pbSerializer getSerializer();
+    public pbDeserializer getDeserializer();
+
+  }
+
+  public class tdSerializer implements pbSerializer{
+    LogGroup lg;
+    public void initial(byte[] bytes){
+      lg = LogGroup.parseFrom(bytes);
+    }
+
+    public String getDspName(){
+      for (TestData.Site site : lg.getSiteList()){
+        return site.getName();
+      }
+      return null;
+    }
+  }
+}
